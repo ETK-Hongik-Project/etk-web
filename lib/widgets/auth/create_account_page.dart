35df900da523ea52a7f83531819a3382480977f1
Min Jeon <mulsoap0504@gmail.com>
2024-08-15 13:10:15 +0900

    Refactor: 예외처리

--- lib/widgets/auth/create_account_page.dart
+++ lib/widgets/auth/create_account_page.dart
@@ -1,5 +1,4 @@
 import 'package:etk_web/api/user.dart';
-import 'package:etk_web/main.dart';
 import 'package:flutter/material.dart';
 
 class CreateAccountPage extends StatefulWidget {
@@ -30,17 +29,35 @@ class _CreateAccountPageState extends State<CreateAccountPage> {
     final String password = _passwordController.text;
     final String email = _emailController.text;
 
+    const String NoUsernameEnteredError = "[username](은)는 아이디가 입력되지 않았습니다";
+    const String NoPasswordEnteredError = "[password](은)는 비밀번호가 입력되지 않았습니다";
+    const String NoNameEnteredError = "[name](은)는 이름이 입력되지 않았습니다";
+    const String NoEmailEnteredError = "[email](은)는 이메일이 입력되지 않았습니다";
+    const String InvalidEmailFormatError = "[email](은)는 올바른 형식이 아닙니다";
+    const String DuplicatedUsernameError = "이미 사용중인 아이디입니다";
+    const String DuplicatedEmailError = "이미 사용중인 이메일입니다";
+
     try {
       await join(name, username, password, email);
     } catch (e) {
-      logger.e(e);
       setState(() {
-        if (e.toString().contains("Duplicated username")) {
-          _errorMessage = "이미 사용중인 아이디입니다.";
-        } else if (e.toString().contains("Duplicated email")) {
-          _errorMessage = "이미 사용중인 이메일입니다.";
-        } else {
-          _errorMessage = "$e";
+        _errorMessage = "회원가입 실패:";
+      });
+      setState(() {
+        if (e.toString().contains(NoNameEnteredError)) {
+          _errorMessage = "$_errorMessage\n이름을 입력해주세요.";
+        } else if (e.toString().contains(NoUsernameEnteredError)) {
+          _errorMessage = "$_errorMessage\n아이디를 입력해주세요.";
+        } else if (e.toString().contains(NoPasswordEnteredError)) {
+          _errorMessage = "$_errorMessage\n비밀번호를 입력해주세요.";
+        } else if (e.toString().contains(NoEmailEnteredError)) {
+          _errorMessage = "$_errorMessage\n이메일을 입력해주세요.";
+        } else if (e.toString().contains(InvalidEmailFormatError)) {
+          _errorMessage = "$_errorMessage\n잘못된 이메일 형식입니다. 이메일 형식을 확인해주세요.";
+        } else if (e.toString().contains(DuplicatedUsernameError)) {
+          _errorMessage = "$_errorMessage\n이미 사용중인 아이디입니다.";
+        } else if (e.toString().contains(DuplicatedEmailError)) {
+          _errorMessage = "$_errorMessage\n이미 사용중인 이메일입니다.";
         }
       });
     }
