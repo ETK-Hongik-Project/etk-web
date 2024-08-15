1bbbb50bc39f2da92eaacda0ba9465538d4e6347
Min Jeon <mulsoap0504@gmail.com>
2024-08-15 14:34:59 +0900

    Refactor: 대화 삭제 수정

--- lib/widgets/keyboard/file_list_screen.dart
+++ lib/widgets/keyboard/file_list_screen.dart
@@ -12,6 +12,8 @@ class FileListScreen extends StatefulWidget {
 
 class _FileListScreenState extends State<FileListScreen> {
   List<FileSystemEntity> _files = [];
+  final Set<FileSystemEntity> _selectedFiles = {};
+  bool _isSelectionMode = false;
 
   @override
   void initState() {
@@ -29,18 +31,87 @@ class _FileListScreenState extends State<FileListScreen> {
     });
   }
 
-  Future<void> _deleteFile(File file) async {
-    try {
-      await file.delete();
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('${file.path.split('/').last} 제거됨')),
-      );
-      _loadFiles(); // Refresh the file list after deletion
-    } catch (e) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('Error deleting file: $e')),
-      );
+  Future<void> _deleteSelectedFiles() async {
+    if (_selectedFiles.isEmpty) {
+      // 선택된 파일이 없으면 선택 모드를 종료
+      setState(() {
+        _isSelectionMode = false;
+      });
+      return;
     }
+
+    // 삭제 확인 다이얼로그 표시
+    bool confirmDelete = await showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: const Text('삭제 확인'),
+              content: Text('${_selectedFiles.length}개의 대화 내역을 삭제하시겠습니까?'),
+              actions: <Widget>[
+                TextButton(
+                  child: const Text('삭제'),
+                  onPressed: () {
+                    Navigator.of(context).pop(true);
+                  },
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                    color: Colors.deepPurpleAccent,
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: TextButton(
+                    child: const Text(
+                      '취소',
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                    onPressed: () {
+                      Navigator.of(context).pop(false);
+                    },
+                  ),
+                ),
+              ],
+            );
+          },
+        ) ??
+        false;
+
+    if (!confirmDelete) {
+      return;
+    }
+
+    for (var file in _selectedFiles) {
+      if (file is File) {
+        try {
+          await file.delete();
+        } catch (e) {
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(content: Text('Error deleting file: $e')),
+          );
+          return;
+        }
+      }
+    }
+
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(content: Text('${_selectedFiles.length}개 대화 내역이 제거되었습니다.')),
+    );
+
+    setState(() {
+      _selectedFiles.clear();
+      _isSelectionMode = false;
+    });
+    _loadFiles();
+  }
+
+  void _toggleSelectionMode() {
+    setState(() {
+      _isSelectionMode = !_isSelectionMode;
+      if (!_isSelectionMode) {
+        _selectedFiles.clear();
+      }
+    });
   }
 
   @override
@@ -54,45 +125,74 @@ class _FileListScreenState extends State<FileListScreen> {
               fontWeight: FontWeight.bold,
               fontSize: 24),
         ),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: IconButton(
+              icon: Icon(_isSelectionMode ? Icons.delete : Icons.delete_outline,
+                  color: _isSelectionMode && _selectedFiles.isNotEmpty
+                      ? Colors.red
+                      : null),
+              onPressed: _isSelectionMode
+                  ? _deleteSelectedFiles
+                  : _toggleSelectionMode,
+            ),
+          ),
+        ],
       ),
       body: ListView.builder(
         itemCount: _files.length,
         itemBuilder: (context, index) {
           final file = _files[index];
+          final fileName = (file.path.split('/').last).split('.').first;
+          final isSelected = _selectedFiles.contains(file);
+
           return ListTile(
             title: Padding(
-              padding: const EdgeInsets.only(left: 4.0),
+              padding: const EdgeInsets.only(left: 5.0),
               child: Text(
-                (file.path.split('/').last).split('.').first,
-                style: const TextStyle(
+                fileName,
+                style: TextStyle(
                   fontSize: 18,
-                  fontWeight: FontWeight.w400,
+                  fontWeight: isSelected ? FontWeight.bold : FontWeight.w400,
+                  color: isSelected ? Colors.red : null,
                 ),
               ),
             ),
-            trailing: IconButton(
-              icon: const Icon(
-                Icons.delete,
-              ),
-              onPressed: () async {
-                if (file is File) {
-                  _deleteFile(file);
-                }
-              },
-            ),
+            leading: _isSelectionMode
+                ? Checkbox(
+                    value: isSelected,
+                    onChanged: (bool? selected) {
+                      setState(() {
+                        if (selected == true) {
+                          _selectedFiles.add(file);
+                        } else {
+                          _selectedFiles.remove(file);
+                        }
+                      });
+                    },
+                  )
+                : null,
             onTap: () async {
-              // 파일 내용을 읽어오는 예제
-              if (file is File) {
+              if (_isSelectionMode) {
+                setState(() {
+                  if (isSelected) {
+                    _selectedFiles.remove(file);
+                  } else {
+                    _selectedFiles.add(file);
+                  }
+                });
+              } else if (file is File) {
                 String contents = await file.readAsString();
                 showDialog(
                   context: context,
                   builder: (context) => AlertDialog(
-                    title: const Text('File Contents'),
+                    title: const Text('대화 내용'),
                     content: Text(contents),
                     actions: [
                       TextButton(
                         onPressed: () => Navigator.pop(context),
-                        child: const Text('OK'),
+                        child: const Text('확인'),
                       ),
                     ],
                   ),
