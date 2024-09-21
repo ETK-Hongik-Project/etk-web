c329f010909309e85d77dcf8588d198aa163f06d
Min Jeon <mulsoap0504@gmail.com>
2024-09-21 17:11:43 +0900

    Refactor: 디자인 수정

--- lib/widgets/keyboard/bottom_text_field.dart
+++ lib/widgets/keyboard/bottom_text_field.dart
@@ -60,6 +60,7 @@ class BottomTextField extends StatelessWidget {
     return Container(
       decoration: BoxDecoration(
         color: Colors.white,
+        borderRadius: BorderRadius.circular(15),
         boxShadow: [
           BoxShadow(
             color: Colors.grey.withOpacity(0.5),
